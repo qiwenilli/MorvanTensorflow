e370e8f18be1323c39cb1adbeb9381dea8848d82
Junhui Zhang <mrlittlezhu@gmail.com>
2017-04-22 22:58:23 +0800

    add code

--- TensorBoard.py
+++ TensorBoard.py
@@ -1,21 +1,31 @@
 import tensorflow as tf
 import numpy as np
 
-def add_layer(inputs, in_size, out_size, activation_function=None):
+def add_layer(inputs, in_size, out_size, n_layer, activation_function=None):
     # add one more layer and return the output of this layer
+    layer_name = 'layer%s' % n_layer
     with tf.name_scope('layer'):
         with tf.name_scope('weights'):
             Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
+            tf.summary.histogram(layer_name + '/weights', Weights)
+
         with tf.name_scope('biases'):
             biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
-        with tf.name_scope('W'):
+            tf.summary.histogram(layer_name + '/biases', biases)
+
+        with tf.name_scope('Wx_plus_b'):
             Wx_plus_b = tf.matmul(inputs, Weights) + biases
+
         if activation_function is None:
             outputs = Wx_plus_b
         else:
             outputs = activation_function(Wx_plus_b)
-        return outputs
 
+        tf.summary.histogram(layer_name + '/outputs', outputs)
+
+    return outputs
+
+# make some real data
 x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
 noise = np.random.normal(0, 0.05, x_data.shape)
 y_data = np.square(x_data) - 0.5 + noise
@@ -26,14 +36,16 @@ with tf.name_scope('inputs'):
     ys = tf.placeholder(tf.float32, [None, 1], name = 'y_input')
 
 # add hidden layer
-l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+l1 = add_layer(xs, 1, 10, n_layer=1, activation_function=tf.nn.relu)
 # add output layer
-prediction = add_layer(l1, 10, 1, activation_function=None)
+prediction = add_layer(l1, 10, 1, n_layer=2, activation_function=None)
 
 # the error between prediction and real data
 with tf.name_scope('loss'):
     loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
                      reduction_indices=[1]))
+    tf.summary.scalar('loss', loss)
+
 with tf.name_scope('train'):
     train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
@@ -52,7 +64,8 @@ for i in range(1000):
     summary = sess.run(train_step, feed_dict={xs:x_data, ys:y_data})
     if i % 50 == 0:
         print(sess.run(loss, feed_dict={xs:x_data, ys:y_data}))
-        train_writer.add_summary(summary, i)
+        result = sess.run(merged, feed_dict={xs: x_data, ys: y_data})
+        train_writer.add_summary(result, i)
         saver.save(sess, "logs/model.ckpt", i)
         print("Adding run metadata for", i)
 train_writer.close()
